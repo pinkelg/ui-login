118ad4b0a5d6f348fca38b7e5d5dd88aa779d6c3
Pinkel Gurung <pinkelgurung@gmail.com>
2022-07-23 22:10:29 -0400

    login form validation

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,50 +1,70 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Link from '@mui/material/Link';
+import { useFormik } from 'formik';
+
+import * as Yup from 'yup';
+
 import { Logger } from '../utils';
 
 import styles from './LoginForm.module.scss';
 
 export const LoginForm = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    onSubmit(values) {
+      Logger.info('LoginForm - OnSubmit', { ...values });
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().label('Email').required().email('Invalid email address'),
+      password: Yup.string().label('Password').min(8, 'Password must be atleast 8 characters').required()
+    })
+  });
 
   useEffect(() => {
     Logger.info('Component - LoginForm UseEffect');
   }, []);
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    Logger.info('LoginForm - OnSubmit', username, password);
-  };
-
   return (
     <div className={styles.container}>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={formik.handleSubmit}>
         <div className={styles.formTitle}>
-          <Typography variant="h4">Login Form</Typography>
+          <Typography variant="h4">Login</Typography>
         </div>
         <div className={styles.formControlsContainer}>
           <TextField
-            data-testid="username"
+            error={!!(formik.touched.email && formik.errors && formik.errors.email)}
+            data-testid="email"
+            name="email"
             variant="outlined"
             className={styles.textField}
-            placeholder="Username/Email"
-            onChange={(e) => setUsername(e.target.value)}
-            value={username}
-            type="email"
+            placeholder="Email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+            helperText={formik.touched.email && formik.errors && formik.errors.email ? formik.errors.email : null}
+            type="text"
           />
 
           <TextField
+            error={!!(formik.touched.password && formik.errors && formik.errors.password)}
             data-testid="password"
+            name="password"
             variant="outlined"
             className={styles.textField}
             placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
             type="password"
+            helperText={
+              formik.touched.password && formik.errors && formik.errors.password ? formik.errors.password : null
+            }
           />
 
           <Button variant="contained" type="submit" className={`${styles.button} ${styles.submitButton}`}>
